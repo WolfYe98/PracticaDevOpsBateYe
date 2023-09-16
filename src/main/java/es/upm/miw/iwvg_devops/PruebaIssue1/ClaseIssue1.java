8ed3a898663c66b2aae8bf9708183fb485a3ba3f
WolfYe98 <mingye@hotmail.es>
2023-09-16 09:39:40 +0200

    modificaciones 2 del issue #4

--- src/main/java/es/upm/miw/iwvg_devops/PruebaIssue1/ClaseIssue1.java
+++ src/main/java/es/upm/miw/iwvg_devops/PruebaIssue1/ClaseIssue1.java
@@ -4,5 +4,6 @@ package es.upm.miw.iwvg_devops.PruebaIssue1;
 // modificaciones de issue 3
 // modificaciones 2 de issue 3
 // modificaciones 1 del issue 4
+// modificaciones 2 del issue 4
 public class ClaseIssue1 {
 }
